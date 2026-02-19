65a85c8e89c663bd9aa0b6148f6244b6950803ec
Femilin Yahini <w.femilinyahini094@gmail.com>
2026-02-19 18:49:16 +0530

    Connected with local MongoDB

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server working");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
